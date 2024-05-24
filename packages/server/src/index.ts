ec277949e3501ea7e6a276bf0468148ae55dbd78
tnraro <deokji53@gmail.com>
2024-05-25 04:24:55 +0900

    feat: use elysia

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import cors from "@elysiajs/cors";
+import swagger from "@elysiajs/swagger";
+import Elysia from "elysia";
+
+const app = new Elysia()
+  .use(cors())
+  .use(swagger())
+  .ws("/ws", {
+    message(ws, message) {
+      ws.send(message)
+    }
+  })
+  .get("/", () => 53)
+  .listen(8080)
+
+export type App = typeof app;
--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -1,1 +1,6 @@
-document.querySelector("#app")!.innerHTML = "53";
+import { treaty } from "@elysiajs/eden";
+import type { App } from "server";
+
+const client = treaty<App>(import.meta.env.VITE_API_ENTRYPOINT);
+
+document.querySelector("#app")!.innerHTML = (await client.index.get()).data + "";
