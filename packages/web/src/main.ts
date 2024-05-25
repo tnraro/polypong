eabf0b94191572204dc21e6968543bdb87db1cc6
tnraro <deokji53@gmail.com>
2024-05-25 20:46:10 +0900

    feat: add client-side code

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -3,4 +3,24 @@ import type { App } from "server";
 
 const client = treaty<App>(import.meta.env.VITE_API_ENTRYPOINT);
 
+const ws = client.ws.subscribe({
+  query: {
+    room: "53",
+  }
+});
+
+ws.on("open", (e) => {
+  console.log(e.type, e)
+  ws.send({ x: 53 });
+});
+ws.subscribe((message) => {
+  console.log(message);
+});
+ws.on("error", (e) => {
+  console.error(e.type, e)
+});
+ws.on("close", (e) => {
+  console.log(e.type, e)
+});
+
 document.querySelector("#app")!.innerHTML = (await client.index.get()).data + "";
