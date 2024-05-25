7286c168b85042b47a261cfc99cc9a65328a1079
tnraro <deokji53@gmail.com>
2024-05-25 20:45:45 +0900

    feat: apply game-manager to server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,16 +1,36 @@
 import cors from "@elysiajs/cors";
 import swagger from "@elysiajs/swagger";
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
+import { GameManager } from "./game-manager";
 
-const app = new Elysia()
+export const app = new Elysia()
   .use(cors())
   .use(swagger())
   .ws("/ws", {
+    query: t.Object({
+      room: t.String(),
+    }),
     message(ws, message) {
-      ws.send(message)
+      console.log(ws.id, message);
+    },
+    open(ws) {
+      gameManager.ws = ws;
+      ws.subscribe(ws.data.query.room);
+      gameManager.add(ws.data.query.room, ws.id);
+    },
+    close(ws) {
+      gameManager.add(ws.data.query.room, ws.id);
+      ws.unsubscribe(ws.data.query.room);
     }
   })
   .get("/", () => 53)
   .listen(8080)
+
+const gameManager = new GameManager(app.server!);
+
+setInterval(() => {
+  gameManager.update(1 / 60);
+  gameManager.publish();
+}, 1000 / 60);
 
 export type App = typeof app;
