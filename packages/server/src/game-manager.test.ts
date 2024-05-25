914e60c69e509dbaac39820d9a705365005b0c96
tnraro <deokji53@gmail.com>
2024-05-25 20:44:25 +0900

    feat: add game-manager

---
+++ packages/server/src/game-manager.test.ts
@@ -0,0 +1,53 @@
+import { describe, test, expect } from "bun:test";
+import { GameManager } from "./game-manager";
+
+describe("GameManager", () => {
+  test("add", () => {
+    const gameManager = new GameManager(null!);
+    gameManager.add("r1", "p1");
+    expect(gameManager.serialize())
+      .toStrictEqual([
+        { id: "r1", game: { players: [{ id: "p1", index: 0, x: 0.5 }], balls: [] } }
+      ]);
+  });
+  test("remove — 1", () => {
+    const gameManager = new GameManager(null!);
+    gameManager.add("r1", "p1");
+    gameManager.add("r1", "p2");
+    gameManager.add("r2", "p3");
+    gameManager.remove("r2", "p3");
+    expect(gameManager.serialize())
+      .toStrictEqual([
+        {
+          id: "r1", game: {
+            players: [
+              { id: "p1", index: 0, x: 0.5 },
+              { id: "p2", index: 1, x: 0.5 }
+            ], balls: []
+          }
+        },
+      ]);
+  });
+  test("remove — 2", () => {
+    const gameManager = new GameManager(null!);
+    gameManager.add("r1", "p1");
+    gameManager.add("r1", "p2");
+    gameManager.add("r2", "p3");
+    gameManager.remove("r1", "p2");
+    expect(gameManager.serialize())
+      .toStrictEqual([
+        { id: "r1", game: { players: [{ id: "p1", index: 0, x: 0.5 }], balls: [] } },
+        { id: "r2", game: { players: [{ id: "p3", index: 0, x: 0.5 }], balls: [] } },
+      ]);
+  });
+  test("remove — 3", () => {
+    const gameManager = new GameManager(null!);
+    gameManager.add("r1", "p1");
+    gameManager.add("r1", "p2");
+    gameManager.add("r2", "p3");
+    gameManager.remove("r1", "p2");
+    gameManager.remove("r2", "p3");
+    gameManager.remove("r1", "p1");
+    expect(gameManager.serialize()).toStrictEqual([]);
+  });
+});
---
+++ packages/server/src/game-manager.ts
@@ -0,0 +1,40 @@
+import type { Server } from "bun";
+import { Game } from "./game";
+
+export class GameManager {
+  ws: any;
+  #games = new Map<string, Game>();
+  #server;
+  constructor(server: Server) {
+    this.#server = server;
+  }
+  add(roomId: string, playerId: string) {
+    const game = this.#games.get(roomId) ?? new Game();
+    game.addPlayer(playerId);
+    this.#games.set(roomId, game);
+  }
+  remove(roomId: string, playerId: string) {
+    const game = this.#games.get(roomId);
+    if (game == null) throw new Error("Game not found");
+    game.removePlayer(playerId);
+    if (game.players.length === 0) {
+      this.#games.delete(roomId);
+    }
+  }
+  update(delta: number) {
+    for (const game of this.#games.values()) {
+      game.update(delta);
+    }
+  }
+  publish() {
+    for (const [id, game] of this.#games) {
+      this.#server.publish(id, JSON.stringify(game.serialize()), true);
+    }
+  }
+  serialize() {
+    return [...this.#games].map(([id, game]) => ({
+      id,
+      game: game.serialize(),
+    }))
+  }
+}
