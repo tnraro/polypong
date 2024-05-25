39a5db93d630f3383bc91da1fc5b857535aa4bc7
tnraro <deokji53@gmail.com>
2024-05-25 20:46:38 +0900

    feat: add game test

---
+++ packages/server/src/game.test.ts
@@ -0,0 +1,23 @@
+import { describe, test, expect } from "bun:test";
+import { Game } from "./game";
+
+describe("Game", () => {
+  test("addPlayer", () => {
+    const game = new Game();
+    game.addPlayer("p1");
+    expect(game.player("p1")?.serialize()).toStrictEqual({
+      id: "p1",
+      x: 0.5,
+    });
+    expect(game.serialize()).toStrictEqual({
+      players: [
+        {
+          id: "p1",
+          x: 0.5,
+          index: 0,
+        }
+      ],
+      balls: [],
+    });
+  });
+});
